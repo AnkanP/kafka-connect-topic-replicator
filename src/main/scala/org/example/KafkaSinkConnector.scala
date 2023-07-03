adea1e5c4c15ff2b3acc6233da2ddd154d9d4d89
crawlerp <ankanpattnaik@gmail.com>
2023-07-03 09:23:23 +0100

    first commit

---
+++ src/main/scala/org/example/KafkaSinkConnector.scala
@@ -0,0 +1,66 @@
+package org.example
+
+import org.apache.kafka.common.config.ConfigDef
+import org.apache.kafka.common.utils.AppInfoParser
+import org.apache.kafka.connect.connector.Task
+import org.apache.kafka.connect.sink.SinkConnector
+import org.slf4j.LoggerFactory
+
+import java.util
+import java.util.Collections
+
+
+ class KafkaSinkConnector extends SinkConnector{
+
+  final val logger = LoggerFactory.getLogger(classOf[KafkaSinkConnector])
+  var connectorConfig: KafkaSinkConfig = null
+  var configProps: util.Map[String, String] = null
+
+  override def start(props: util.Map[String, String]): Unit = {
+
+   // When the connector starts, this method is called which provides a new instance of our custom configuration to the kafka connect framework
+    this.connectorConfig =  new KafkaSinkConfig(props)
+
+    this.configProps = Collections.unmodifiableMap(props)
+
+  }
+
+  override def taskClass(): Class[_ <: Task] = classOf[KafkaSinkTask]
+
+  //3  It just has to determine the number of input tasks, which may require contacting the remote service it is pulling data from, and then divvy them up
+  //which returns a list of maps containing the configuration properties each task will use to stream data into or out of Kafka:
+  //The method accepts an int value for the maximum number of tasks to run in parallel and is pulled from the tasks.max configuration property that is provided on startup.
+  //https://www.confluent.io/blog/create-dynamic-kafka-connect-source-connectors/
+
+  //On startup, the Kafka Connect framework will pass each configuration map contained in the list returned by taskConfigs to a task.
+
+
+   override def taskConfigs(maxTasks: Int): util.List[util.Map[String, String]] = {
+     val configs = new util.ArrayList[util.Map[String, String]]
+
+     for (i <- 0 until maxTasks) {
+
+       // Only one input partition makes sense.
+       val config = new util.HashMap[String, String]()
+       config.putAll(configProps)
+       configs.add(config)
+
+     }
+
+     configs
+   }
+
+   override def stop(): Unit = {
+    // Nothing to do since FileStreamSinkConnector has no background monitoring.
+  }
+
+
+   // The below method ensures the validation happens while submitting the connect job.
+  override def config(): ConfigDef = {
+    KafkaSinkConfig.CONFIG_DEF
+  }
+
+  override def version(): String = AppInfoParser.getVersion
+
+
+}
---
+++ src/main/scala/org/example/KafkaSinkTask.scala
@@ -0,0 +1,116 @@
+package org.example
+
+import io.confluent.connect.avro.{AvroData, AvroDataConfig}
+import org.apache.kafka.clients.consumer.OffsetAndMetadata
+import org.apache.kafka.clients.producer._
+import org.apache.kafka.common.TopicPartition
+import org.apache.kafka.common.header.internals.RecordHeader
+import org.apache.kafka.connect.sink.{SinkRecord, SinkTask}
+import org.slf4j.LoggerFactory
+
+import java.util
+import java.util.Properties
+import scala.collection.JavaConversions._
+
+
+class KafkaSinkTask extends SinkTask{
+
+  private val log = LoggerFactory.getLogger(classOf[KafkaSinkTask])
+
+  var topic: String = ""
+  var producerConfig: String = ""
+  var bootstrapServers: String = ""
+  var schemaRegistryUrl: String = ""
+
+  /* lazy objects get evaluated at variable call */
+    lazy val properties: Properties = buildProperties
+    lazy val kafkaProducer: KafkaProducer[Object, Object] = new KafkaProducer(this.properties)
+
+
+  //3. Send the record to kafka
+ lazy val callback: Callback = new Callback {
+    override def onCompletion(metadata: RecordMetadata, exception: Exception): Unit = {
+      Option(exception) match {
+        case Some(e) => println(s"Failed to produce: ${e.printStackTrace()}")
+        case None => println(s"Produced record at $metadata")
+      }
+    }
+  }
+
+
+  override def start(map: util.Map[String, String]): Unit = {
+
+    this.topic = map.get(KafkaSinkConfig.DESTINATION_TOPIC)
+    this.producerConfig = map.get(KafkaSinkConfig.PRODUCER_CONFIG)
+    this.bootstrapServers = map.get(KafkaSinkConfig.DESTINATION_BOOTSTRAP_SERVERS)
+    this.schemaRegistryUrl = map.get(KafkaSinkConfig.DESTINATION_SCHEMA_REGISTRY_URL)
+  }
+
+  override def put(collection: util.Collection[SinkRecord]): Unit = {
+
+    for (record <- collection) {
+      log.info("record : {}",  record.value)
+
+
+      //connect schema
+      val valSchema = record.valueSchema()
+      val keySchema = record.keySchema()
+
+     val avroData = new AvroData(new AvroDataConfig(this.properties))
+
+      //convert connect schema to avro schema
+      //val avroValSchema = avroData.fromConnectSchema(valSchema)
+      //val avroKeySchema = avroData.fromConnectSchema(keySchema)
+
+      //convert to connect record to avro object
+      val valObj: Object = avroData.fromConnectData(valSchema,record.value())
+      val keyObj: Object = avroData.fromConnectData(keySchema,record.key())
+
+
+      //4. Send producer record
+
+      val producerRecord = new ProducerRecord[Object, Object](topic, keyObj, valObj)
+
+
+      // Add headers
+      for(header <- record.headers()){
+        producerRecord.headers().add(new RecordHeader(header.key(), header.value().asInstanceOf[Array[Byte]]))
+      }
+
+      //producerRecord.headers().add(new RecordHeader("timestamp", System.currentTimeMillis().toString.getBytes()))
+      this.kafkaProducer.send(producerRecord,callback)
+    }
+  }
+
+  override def stop(): Unit = {
+    log.info("Shutting down kafka producer!!")
+    this.kafkaProducer.close()
+  }
+
+  override def version(): String = {
+    new KafkaSinkConnector().version()
+  }
+
+  override def flush(currentOffsets: util.Map[TopicPartition, OffsetAndMetadata]): Unit = {
+    log.info("Flushing kafka producer for {}")
+    this.kafkaProducer.flush()
+  }
+
+  def buildProperties: Properties = {
+    val props: Properties = new Properties
+
+    val hashMap = this.producerConfig.split(',').map(_.split("=")).map {case Array(k,v) => (k,v) }.toMap
+
+    val javaMap: util.Map[_,_] = mapAsJavaMap(hashMap)
+
+    props.putAll(javaMap)
+
+    props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, this.bootstrapServers)
+    props.setProperty("schema.registry.url", this.schemaRegistryUrl)
+
+    props
+  }
+
+
+}
+
